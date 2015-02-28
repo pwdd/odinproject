f9c53f8928c01cbfeecceb00d1eac81d4fcff78d
pwdd <pinto.wdesign@gmail.com>
2015-02-27 19:16:11 -0800

    work on hover changing color of background

--- sketchpad/js/scripts.js
+++ sketchpad/js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	for (var i = 1; i <= 16; i++) {
-		$('#container').append('<div id="box-' + i + '"></div>');
+		$('#container').append('<div id="box-' + i + '" class="grid"></div>');
+		$('.grid').mouseenter(function() {
+			$('.grid').addClass('blue');
+		});
 	}
 });
