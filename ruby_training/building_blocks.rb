177e043210806afac8bc3113fea412d69bddac7e
pwdd <pinto.wdesign@gmail.com>
2015-04-29 01:04:18 -0700

    add substrings

--- ruby_training/building_blocks.rb
+++ ruby_training/building_blocks.rb
@@ -50,7 +50,7 @@ def stock_picker(prices)
 			if profit < sell - buy
 				profit = sell - buy
 				price_to_buy = index_buy
-				price_to_sell = index_sell + (index_buy + 1) #fix it!
+				price_to_sell = index_sell + (index_buy + 1)
 			end
 		end
 	end
@@ -59,12 +59,27 @@ end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
 
+=begin
+Implement a method #substrings that takes a word as the first argument and then an array of valid substrings 
+(your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) 
+that was found in the original string and how many times it was found.
+=end
 
+def substrings(string, dictionary)
+	result = Hash.new(0)
+	arr = string.split(" ")
+	arr.each do |word|
+		dictionary.each do |sub|
+			result[sub] += 1 if word[sub]
+		end
+	end
+	result
+end
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-
-
-
+print substrings("Howdy partner, sit down! How's it going?", dictionary)
+print substrings("below", dictionary)
 
 
 
