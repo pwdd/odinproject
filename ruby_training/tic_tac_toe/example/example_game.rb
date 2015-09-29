f2f5b8786012fa7b4d5bf7a38d738b777c0c9749
pwdd <aina@pintowebdevelopment.com>
2015-09-29 16:19:28 -0700

    finished

---
+++ ruby_training/tic_tac_toe/example/example_game.rb
@@ -0,0 +1,9 @@
+
+root = File.expand_path("../", File.dirname(__FILE__))
+require "#{root}/lib/tic_tac_toe.rb"
+ 
+puts "Welcome to tic tac toe"
+bob = TicTacToe::Player.new({letter: "X", name: "bob"})
+frank = TicTacToe::Player.new({letter: "O", name: "frank"})
+players = [bob, frank]
+TicTacToe::Game.new(players).play
